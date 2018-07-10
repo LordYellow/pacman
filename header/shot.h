b3269c3fcab3a2c2fd481546e7e239585e252f69
LordYellow <raphaelstraub@live.de>
2018-07-10 16:23:28 +0200

    automated update

--- header/shot.h
+++ header/shot.h
@@ -15,10 +15,14 @@ public:
     
     fieldtyp<uint8_t> *field;
     bool viableShot = true;
-    uint8_t y, x, targetY, targetX, baseDelay = 5, delayCounter = 0, lastfield = ROAD;
+    uint8_t y, x;
+    uint8_t targetY, targetX, baseDelay = 5, delayCounter = 0, lastfield = ROAD;
     array<string, 8> *colors;
+    vector<array<uint8_t, 2>> pointVector;
     
     void move();
+    
+    void getShotFields();
 };
 
 shot::shot(uint8_t y, uint8_t x, uint8_t targetY, uint8_t targetX, array<string, 8> *thecolors){
@@ -27,16 +31,24 @@ shot::shot(uint8_t y, uint8_t x, uint8_t targetY, uint8_t targetX, array<string,
     this->targetY = targetY;
     this->targetX = targetX;
     this->colors = thecolors;
+    this->getShotFields();
+}
+
+void shot::getShotFields(){
+    double unitVectorY = (this->targetY-this->y)/sqrt(pow(this->targetY-this->y, 2) + pow(this->targetX-this->x, 2)), unitVectorX = (this->targetX-this->x)/sqrt(pow(this->targetY-this->y, 2) + pow(this->targetX-this->x, 2));
+    for(uint8_t i = 0;; i++){
+        if(round(this->y + unitVectorY*i) == this->targetY && round(this->x + unitVectorX*i) == this->targetX) break;
+        this->pointVector.push_back({(uint8_t)round(this->y + unitVectorY*i), (uint8_t)round(this->x + unitVectorX*i)});
+    }
 }
 
 void shot::move(){ //will rewrite it tomorrow 
     if(this->delayCounter == this->baseDelay){
         this->delayCounter = 0;
-        drawWithTexture(x, y, this->colors, (*this->field)[y][x]);
-        this->x-= (this->x-this->targetX)/sqrt(pow(this->y-this->targetY, 2) + pow(this->x-this->targetX, 2));
-        this->y-= (this->y-this->targetY)/sqrt(pow(this->y-this->targetY, 2) + pow(this->x-this->targetX, 2));
-        if((*this->field)[y][x] == WALL){this->viableShot = false; return;}
-        draw(x, y, "\033[48;5;13m* ");
+        drawWithTexture(this->pointVector[0][1], this->pointVector[0][0], this->colors, (*this->field)[this->pointVector[0][1]][this->pointVector[0][0]]); // for some reason this will cause a segmentation fault
+        pointVector.erase(pointVector.begin());
+        if((*this->field)[this->pointVector[0][1]][this->pointVector[0][0]] == WALL || this->y > HIGH || this->x > WIDTH || pointVector.size() == 0){this->viableShot = false; return;}
+        draw(this->pointVector[0][1], this->pointVector[0][0], "\033[48;5;13m* ");
     }else{
         this->delayCounter++;
     }
